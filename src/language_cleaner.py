f0da3daca38e6d685eb11376e9748f6131e62a7e
Apoorv Sharma <sharmava@uw.edu>
2021-02-21 09:27:21 -0800

    Added parser for Wortschatz

--- src/language_cleaner.py
+++ src/language_cleaner.py
@@ -114,3 +114,68 @@ class RussianCleaner(LanguageCleaner):
         
         sentence_list = map(str.strip, sentence_list)
         return list(sentence_list)
+
+class WortschatzLanguageParser():
+    def __init__(self, sentences_file_path, save_file_path="", save_sentences=False) -> None:
+
+        if os.path.isfile(save_file_path):
+            self.sentence_list = self.read_sentence_list_from_file(save_file_path)
+            return
+
+        if not os.path.exists(sentences_file_path):
+            print(f"Error! File Path {sentences_file_path} not found")
+            return None
+        
+        sentences_list = []
+        with open(sentences_file_path, "r") as file:
+            sentences_list = file.readlines()
+        
+        cleaned_sentence = []
+        for sentence in sentences_list:
+            sentence = sentence.split('\t')[1].strip()
+            
+            if len(sentence) >= 5:
+                cleaned_sentence.append(self.clean_sentence(sentence))
+
+        self.sentence_list = cleaned_sentence
+
+        if save_sentences:
+            self.save_sentence_list_to_file(save_file_path)
+
+    
+    def clean_sentence(self, text):
+        # remove numbers
+        text_nonum = re.sub(r'\d+', '', text)
+
+        # text_no_punct = text_nonum.translate(str.maketrans('', '', string.punctuation))
+        punctuationNoPeriod = "[" + re.sub("\.","", string.punctuation) + "]"
+        text_no_punct = re.sub(punctuationNoPeriod, "", text_nonum)
+
+        # substitute multiple whitespace with single whitespace
+        # Also, removes leading and trailing whitespaces
+        text_no_doublespace = re.sub('\s+', ' ', text_no_punct).strip()
+        return text_no_doublespace
+    
+    def get_sentence_list(self) -> list:
+        return self.sentence_list
+    
+    def save_sentence_list_to_file(self, filePath):
+        with open(filePath, 'w') as fh:
+            for sentence in self.sentence_list:
+                fh.write('%s\n' % sentence)
+    
+    def read_sentence_list_from_file(self, filePath):
+        sentence_list = []
+
+        # open file and read the content in a list
+        with open(filePath, 'r') as fh:
+            sentence_list = fh.readlines()
+        
+        sentence_list = map(str.strip, sentence_list)
+        return list(sentence_list)
+        
+if __name__ == '__main__':
+    arabic = WortschatzLanguageParser('data/ara_news_2017_1M-sentences.txt', 'data/arabic.txt', True)
+    dutch = WortschatzLanguageParser('data/deu_mixed-typical_2011_1M-sentences.txt', 'data/dutch.txt', True)
+    french = WortschatzLanguageParser('data/fra_newscrawl-public_2019_1M-sentences.txt', 'data/french.txt', True)
+    luxemborgish = WortschatzLanguageParser('data/ltz-lu_web_2013_1M-sentences.txt', 'data/luxemborgish.txt', True)
